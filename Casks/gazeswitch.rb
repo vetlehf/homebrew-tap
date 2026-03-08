7ca889e567863a42c328022fdf860ddf7a3372a8
vhfsec <110307925+vhfsec@users.noreply.github.com>
2026-03-08 23:24:54 +0100

    chore: update sha256 for notarized v1.1.0 DMG

--- Casks/gazeswitch.rb
+++ Casks/gazeswitch.rb
@@ -1,6 +1,6 @@
 cask "gazeswitch" do
   version "1.1.0"
-  sha256 "eac290082fd8b7783006325df5c858fd92f5680b5e0d0c35351d5373f7055f26"
+  sha256 "ea6e2504316b954108094d2fdec6bcee78d72a55a97ddfa44f8c6c91da6a5990"
 
   url "https://github.com/vetlehf/gazeswitch/releases/download/v#{version}/GazeSwitch-v#{version}.dmg"
   name "GazeSwitch"
